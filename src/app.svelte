22dbaab84b5fa74d29d3915ef2ba4d18ec4001fa
Pat Cavit <github@patcavit.com>
2018-06-20 00:02:45 -0700

    wip: totally broken on windows

---
+++ src/app.svelte
@@ -0,0 +1,38 @@
+<h1>HI</h1>
+
+<script>
+    export default {
+        oncreate() {
+            console.log("Created!");
+        }
+    };
+    // import { Machine } from "xstate";
+
+    // const machine = Machine({
+    //     key: 'light',
+    //     initial: 'green',
+    //     states: {
+    //         green: {
+    //             on: {
+    //                 TIMER: 'yellow',
+    //             }
+    //         },
+    //         yellow: {
+    //             on: {
+    //                 TIMER: 'red',
+    //             }
+    //         },
+    //         red: {
+    //             on: {
+    //                 TIMER: 'green',
+    //             }
+    //         }
+    //     }
+    // });
+
+    // let state = machine.initialState;
+
+    // const next = machine.transition(current, "TIMER");
+
+    // console.log(next);
+</script>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from "./app.svelte";
+
+console.log(App);
+
+new App({
+    target : document.body
+});
