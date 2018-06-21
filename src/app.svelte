91151ecd98d7eea30fb4438be715df3f7dcf060b
Pat Cavit <github@patcavit.com>
2018-06-20 22:16:24 -0700

    get things working (sorta)
    Broken pending https://github.com/DeMoorJasper/parcel-plugin-svelte/pull/28

--- src/app.svelte
+++ src/app.svelte
@@ -1,38 +1,37 @@
 <h1>HI</h1>
 
 <script>
-    export default {
-        oncreate() {
-            console.log("Created!");
+    import { Machine } from "xstate";
+
+    const machine = Machine({
+        key: 'light',
+        initial: 'green',
+        states: {
+            green: {
+                on: {
+                    TIMER: 'yellow',
+                }
+            },
+            yellow: {
+                on: {
+                    TIMER: 'red',
+                }
+            },
+            red: {
+                on: {
+                    TIMER: 'green',
+                }
+            }
         }
-    };
-    // import { Machine } from "xstate";
+    });
 
-    // const machine = Machine({
-    //     key: 'light',
-    //     initial: 'green',
-    //     states: {
-    //         green: {
-    //             on: {
-    //                 TIMER: 'yellow',
-    //             }
-    //         },
-    //         yellow: {
-    //             on: {
-    //                 TIMER: 'red',
-    //             }
-    //         },
-    //         red: {
-    //             on: {
-    //                 TIMER: 'green',
-    //             }
-    //         }
-    //     }
-    // });
+    let state = machine.initialState;
 
-    // let state = machine.initialState;
+    let next = machine.transition(state, "TIMER");
 
-    // const next = machine.transition(current, "TIMER");
+    console.log(next);
 
-    // console.log(next);
+    next = machine.transition(next, "TIMER");
+
+    console.log(next);
 </script>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import App from "./app.svelte";
 
-console.log(App);
-
 new App({
     target : document.body
 });
