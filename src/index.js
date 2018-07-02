83268836afd4b90e8d7a2594391b79596f3a8f5d
Pat Cavit <github@patcavit.com>
2018-07-01 22:48:13 -0700

    Sapper nested component semantics
    From https://github.com/sveltejs/sapper/issues/262#issuecomment-401643076
    
    Doesn't actually support any props yet though, not sure how I want to set those up just yet.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
+import state from "./state.js";
+
 import App from "./app.html";
 
-new App({
-    target : document.body,
+let app;
+
+state.on("enter", ({ curr }) => {
+    // TODO: Assumes single-level component at the root
+    if(!app) {
+        app = new App({
+            target : document.body,
+            data   : {
+                page : {
+                    child : curr.component,
+                    props : {},
+                },
+            },
+        });
+
+        return;
+    }
+
+    // Single level component
+    if(!curr.parent) {
+        app.set({
+            page : {
+                child : curr.component,
+                props : {},
+            },
+        });
+
+        return;
+    }
+
+    // Create the list of nested components
+    const components = [];
+    let step = curr;
+
+    while(step.parent) {
+        components.push(step);
+
+        step = state.get(step.parent);
+    }
+
+    components.push(step);
+
+    // Create nested data structure to represent the component tree
+    const data = {};
+
+    components.reverse().reduce((prev, component) => {
+        prev.page = {
+            child : component.component,
+            props : {},
+        };
+
+        return prev.page.props;
+    }, data);
+
+    app.set(data);
 });
+
+state.start();
